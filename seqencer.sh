e9dcef392762d6e14c7d3cb49c8b2307276afcef
Jure Sorn <sornjure@hotmail.com>
2013-10-24 01:02:24 +0200

    bla

--- seqencer.sh
+++ seqencer.sh
@@ -1,5 +1,26 @@
-for I in {1..5}; do
-	cat genome"$I" | tr -d "\n" | grep "$1" -Eo | sed "$2" | sort | uniq -c | sort -gr > ./tmp/tmp"$I"
+N=5
+
+rm tmp/*
+
+for ((i=1; i<=$N; i++)); do
+	cat genome"$i" | tr -d "\n" | grep "$1" -Eo | sed "$2" | sort | uniq -c | sort -gr > ./tmp/tmp"$i"
 done
 
-paste `for I in {1..5}; do echo ./tmp/tmp"$I"; done`
+#make temp files the same length
+maxLines=`wc -l tmp/* | grep -v total | sed 's/^[^0-9]*\([0-9]*\).*/\1/' | sort -n | tail -n1`
+for f in tmp/*; do
+	echo "$f"
+	tmpLines=`wc -l "$f" | sed 's/^[^0-9]*\([0-9]*\).*/\1/'`
+	#echo "$maxLines -"
+	#echo "$tmpLines ="
+	let "delta = $maxLines - $tmpLines"
+	echo "$delta"
+	out=""
+	for ((i=1; i<=$delta; i++)); do
+		out="$out\n\t\t"
+	done
+	echo "$out"
+	echo -e "$out" >> "$f"
+done
+
+paste $(column -t $(for ((i=1; i<=$N; i++)); do echo ./tmp/tmp"$i"; done))
