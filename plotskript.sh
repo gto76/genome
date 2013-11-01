10e63a432c603ce6bbb021b4017132db64765635
Jure Sorn <sornjure@hotmail.com>
2013-11-01 20:26:15 +0100

    regular commit

--- plotskript.sh
+++ plotskript.sh
@@ -9,7 +9,8 @@ echo $(
 	echo -n "set xtics("
 	echo -n $(
 		i=0
-		kombinacije=`grep tmp/tmp1 -o -e [ACGT]*`
+		first=`echo $1 | grep -o ^[0-9]*`
+		kombinacije=`grep tmp/tmp$first -o -e [ACGT]*`
 		for komb in $kombinacije; do
 			echo -n "\"$komb\" $i, "
 			let 'i = i + 1'
@@ -24,7 +25,6 @@ echo $(
 	done
 	) | head -c-1
 	echo -n "; "
-	#echo -n "plot 'tmp/tmp1' title \"Skorpijon\", 'tmp/tmp2' title \"Kacica\", 'tmp/tmp3' title \"Clovek\", 'tmp/tmp4' title \"Kobilica\", 'tmp/tmp5' title \"Ligenj\", 'tmp/tmp6' title \"Stonoga\", 'tmp/tmp7' title \"Zaba\", 'tmp/tmp9' title \"Riba\", 'tmp/tmp10' title \"Meduza\", 'tmp/tmp11' title \"Morska Zvezda\"; "
 	echo "replot"
-)  | gnuplot -p
+) | gnuplot -p
 
